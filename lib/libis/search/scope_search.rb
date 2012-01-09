62515403c02c07a674b544fda08420de69a8c59e
Kris Dekeyser <kris.dekeyser@libis.be>
2012-01-09 11:35:52 +0100

    New option: seach in Scope (WIP)

--- lib/libis/search/scope_search.rb
+++ lib/libis/search/scope_search.rb
@@ -1,11 +1,15 @@
 # coding: utf-8
 
+require 'tools/oracle_client'
+require 'tools/xml_document'
+
 class ScopeSearch < GenericSearch
   def initialize
   end
 
   def query(term, index = nil, base = nil, options = {})
-    system "sqlplus APLKN_ARCHV_LIAS/archvsc@SCOPE01 @'#{@metadata_sql_file}'"
+    OracleClient.scope_client.call('kul_packages.scope_xml_meta_file_ed', [term])
+    doc = XmlDocument.open("/nas/vol03/oracle/scope01/#{term}_md.xml")
   end
 
 end
