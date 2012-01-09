10fa508707138965139ed89add8505e0a1b34b60
Kris Dekeyser <kris.dekeyser@libis.be>
2012-01-09 11:34:39 +0100

    several small enhancements and fixes code reorg and cleanup

--- lib/libis/record/sharepoint_record.rb
+++ lib/libis/record/sharepoint_record.rb
@@ -1,29 +1,121 @@
 # coding: utf-8
 
+require 'uri'
+
 require 'tools/hash'
 require 'tools/xml_document'
 
 class SharepointRecord < Hash
   
-  REF_MAPPER = {
-      :content_type => :ows_ContentType,
-      :ar_model => :ows_Access_x0020_rights_x0020_model,
-      :ingest_model => :ows_Ingestmodel,
-      :file_path => :ows_FileRef,
-      :file_name => :ows_FileLeafRef,
-      :dir_name => :ows_FileDirRef,
-      :relative_path => :ows_Referentie, ## this field seems to be unreliable in the sample data
-      :url => :ows_EncodedAbsUrl,
-      :base_name => :ows_BaseName
-  }
-  
-  DTL_MAPPER = {
-      :label => :ows_BaseName,
-      :name =>  :own_BaseName
-  }
-
   def initialize
-    super
+    super nil
+  end
+
+  def label
+    self[:ows_Title1] || self[:ows_BaseName]
+  end
+
+  def content_type()
+    self[:ows_ContentType]
+  end
+
+  def file_name()
+    self[:ows_FileLeafRef]
+  end
+
+  def file_path()
+    self[:ows_FileRef]
+  end
+
+  def file_size()
+    self[:ows_FileSizeDisplay]
+  end
+
+  def url()
+#    self[:ows_EncodedAbsUrl]
+#    'https://www.groupware.kuleuven.be' + URI.escape(self[:ows_ServerUrl], Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+#    'https://www.groupware.kuleuven.be' + URI.escape(self[:ows_ServerUrl])
+    URI.escape(URI.unescape(self[:ows_EncodedAbsUrl]))
+  end
+
+  def relative_path()
+    return file_path.gsub(/^sites\/lias\/Gedeelde documenten\//, '') if file_path
+    nil
+  end
+
+  def local_path(sub_dir)
+    return relative_path unless sub_dir
+    sub_dir += '/' unless sub_dir[-1] == '/'
+    return relative_path.gsub(/^#{sub_dir}/, '') if relative_path
+    nil
+  end
+
+  def is_file?()
+    return true if [:file, :mfile].include? simple_content_type()
+    false
+  end
+
+  def is_described?
+    return false unless self[:ows_Title1] or
+        self[:ows_Creation_x0020_date_x0028_s_x0029_] or
+        self[:ows_Startdate] or
+        self[:ows_Enddate]
+    true
+  end
+
+  def simple_content_type()
+    case content_type
+      when /^Archief/i
+        return :archive
+      when /^Bestanddeel \(folder\)/i
+        return :map
+      when /^Bestanddeel of stuk \(document\)/i
+        return :file
+      when /^Meervoudige beschrijving \(folder\)/i
+        return :mmap
+      when /^Meervoudige beschrijving \(document\)/i
+        return :mfile
+      when /^Tussenniveau/i
+        return :map
+      when /^Film/i
+        return :file
+      when /^Object/i
+        return :file
+      when /^Document/i
+        return :file
+    end
+    :unknown
+  end
+
+  def content_code()
+    case simple_content_type
+      when :archive
+        return 'a'
+      when :map
+        return 'm'
+      when :file
+        return 'f'
+      when :mmap
+        return 'v'
+      when :mfile
+        return '<'
+      when :unknown
+        return '-'
+    end
+    ' '
+  end
+
+  def ingest_model()
+    result = 'Archiveren zonder manifestations'
+    if model = self[:ows_Ingestmodel]
+      case model
+        when 'jpg-watermark_jp2_tn'
+          result = 'Afbeeldingen hoge kwaliteit'
+        when 'jpg-watermark_jpg_tn'
+          result = 'Afbeeldingen lage kwaliteit'
+      end
+    end
+    result
   end
 
   def to_raw
@@ -70,136 +162,46 @@ class SharepointRecord < Hash
     xml_doc.root = xml_doc.create_node(
         'record',
         namespaces: {
-            'dc' => 'http://purl.org/dc/elements/1.1',
+            'dc' => 'http://purl.org/dc/elements/1.1/',
             'xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
-            'dcterms' => 'http://purl.org/dc/terms'})
+            'dcterms' => 'http://purl.org/dc/terms/'})
     
     self.each do |label, value|
-      dc_tag = mapping.dc_tag label
+      dc_tag = mapping.dc_tag(label)
       next unless dc_tag
       dc_value = (mapping.dc_prefix( label ) || '') + value + (mapping.dc_postfix( label ) || '')
       #noinspection RubyResolve
       xml_doc.root << xml_doc.create_text_node(dc_tag, dc_value)
     end
+
+    if xml_doc.xpath('//dc:title').size == 0
+      xml_doc.root << xml_doc.create_text_node('dc:title', self[:ows_BaseName])
+    end
     
     xml_doc
     
   end
+
+  def to_sql( mapping )
+    sql_fields = []
+    sql_values = []
+
+    self.each do |label, value|
+      db_column = mapping.db_column(label)
+      next unless db_column
+      db_value = mapping.db_value(label,value)
+      next unless db_value and db_value != "''"
+      sql_fields << db_column
+      sql_values << db_value.escape_for_sql
+    end
+
+    sql_fields.each_with_index { | element, index | (index % 10 == 0) && sql_fields[index] = "\n    " + element}
+    sql_values.each_with_index { | element, index | (index % 10 == 0) && sql_values[index] = "\n    " + element}
+
+    'INSERT INTO @TABLE_NAME@ (' + sql_fields.join(',') + ")\n  VALUES (" + sql_values.join(',') + ');'
+
+  end
   
-  def [](key)
-    result = super
-    unless result
-      result = super(REF_MAPPER[key])
-    end
-    result
-  end
-
-=begin
-  def node()
-    self[:node]
-  end
-
-  def node=(n)
-    self[:node] = n
-  end
-=end
-
-  def relative_path()
-    full_path = self[:file_path]
-    return full_path.gsub(/^sites\/lias\/Gedeelde documenten\//, '') if full_path
-    nil
-  end
-
-  def local_path(sub_dir)
-    return relative_path unless sub_dir
-    sub_dir += '/' unless sub_dir[-1] == '/'
-    return relative_path.gsub(/^#{sub_dir}/, '') if relative_path
-    nil
-  end
-
-  def file_name()
-    self[:file_name]
-  end
-
-  def file_path()
-    self[:file_path]
-  end
-
-  def url()
-    self[REF_MAPPER[:url]]
-  end
-
-  def is_file?()
-    return true if [:simple, :file, :mfile].include? content_type()
-    false
-  end
-
-  def is_described?
-    return false if ((self[:ows_Title1] and self[:ows_Title1].empty?) or self[:ows_Title1] == self[:FileLeafRef]) and
-        self[:ows_Creation_x0020_date_x0020_from_x0020__x0028_approx_x002e__x0029_] and
-        self[:ows_Creation_x0020_date_x0020_from_x0020__x0028_approx_x002e__x0029_].empty? and
-        self[:ows_Creation_x0020_date_x0020_to_x0020__x0028_approx_x002e__x0029_] and
-        self[:ows_Creation_x0020_date_x0020_to_x0020__x0028_approx_x002e__x0029_].empty? and
-        self[:ows_Creation_x0020_date_x0028_s_x0029_] and
-        self[:ows_Creation_x0020_date_x0028_s_x0029_].empty?
-    true
-  end
-
-  def content_type()
-    case self[:content_type]
-      when /^Archief/i
-        return :archive
-      when /^Bestanddeel \(folder\)/i
-        return :map
-      when /^Bestanddeel of stuk \(document\)/i
-        return :file
-      when /^Meervoudige beschrijving \(folder\)/i
-        return :mmap
-      when /^Meervoudige beschrijving \(document\)/i
-        return :mfile
-      when /^Tussenniveau/i
-        return :map
-      when /^Film/i
-        return :file
-      when /^Object/i
-        return :file
-      when /^Document/i
-        return :file
-    end
-    :unknown
-  end
-
-  def content_code()
-    case content_type
-      when :archive
-        return 'a'
-      when :map
-        return 'm'
-      when :file
-        return 'f'
-      when :mmap
-        return 'v'
-      when :mfile
-        return '<'
-      when :unknown
-        return '-'
-    end
-    ' '
-  end
-
-  def ingest_model()
-    result = 'Archiveren zonder manifestations'
-    if model = self[REF_MAPPER[:ingest_model]]
-      case model
-        when 'jpg-watermark_jp2_tn'
-          result = 'Afbeeldingen hoge kwaliteit'
-        when 'jpg-watermark_jpg_tn'
-          result = 'Afbeeldingen lage kwaliteit'
-      end
-    end
-    result
-  end
-
   def create_dc(dir, mapping)
     xml_doc = to_dc mapping
     dc_file = "#{dir}/dc_#{self[:index].to_s}.xml"
@@ -216,11 +218,8 @@ class SharepointRecord < Hash
     self.each do |label, value|
       next if label == :node
 #      next if label == :index
-      name = mapping.name(label) || label.to_s
-      if ref = REF_MAPPER.invert[label]
-        name += " [#{ref.to_s}]"
-      end
-      f.printf " %38s : %s\n", name, value
+      name = mapping.fancy_label(label)
+      f.printf " %40s : %s\n", name, value
     end
 
   end
