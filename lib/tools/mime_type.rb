7056ffa5477227e38c95e264b95befd50b9bce93
Kris Dekeyser <kris.dekeyser@libis.be>
2012-01-09 11:42:41 +0100

    Added FIDO support Added types for MSOffice 2007 document formats

--- lib/tools/mime_type.rb
+++ lib/tools/mime_type.rb
@@ -1,22 +1,39 @@
 # coding: utf-8
 
+require 'tools/string'
+require 'faster_csv'
+
 class MimeType
 
   def self.get( file_path )
-    fp = file_path.to_s.gsub(/['"\s\[\](){}]/) { |s| '\\' + s[0].to_s }
-    result = %x(file -ib #{fp}).split[0]
-    if result.eql?('application/octet-stream')
-      begin
-        x = %x(identify -format "%m" #{fp})
-        x = x.split[0] if x
-        x = x.strip if x
-        result = 'image/jp2' if x.eql?('JP2')
-      rescue Exception
-        @@logger.warn(self.class) {"Could not identify MIME type of '#{file_path.to_s}''"}
-      end
+
+    # first attempt: use FIDO
+
+    fp = file_path.to_s.escape_for_string
+    result = %x(fido -loadformats #{Application.dir}/config/lias_formats.xml "#{fp}")
+    r = FasterCSV.parse(result)[0]
+    status = r[0]
+    format = r[2]
+    mimetype = r[7]
+    return mimetype if status == "OK" && mimetype != "None"
+
+    # second attempt: use FILE
+    result = %x(file -ib "#{fp}").split[0]
+    return result unless result.eql?('application/octet-stream')
+
+    # final attempt: use ImageMagik's identify
+    begin
+      x = %x(identify -format "%m" #{fp})
+      x = x.split[0] if x
+      x = x.strip if x
+      result = 'image/jp2' if x.eql?('JP2')
+    rescue Exception
+      @@logger.warn(self.class) {"Could not identify MIME type of '#{file_path.to_s}''"}
+      result = ""
     end
-    result = result[0...-1] if result[-1] == ';'
-    result
+
+    result[-1] == ';' ? result[0...-1] : result
+
   end
   
 end
