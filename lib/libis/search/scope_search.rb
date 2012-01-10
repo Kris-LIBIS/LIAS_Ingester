63130400e15b7aabb722da1593f3c6727827a959
Kris Dekeyser <kris.dekeyser@libis.be>
2012-01-10 17:29:57 +0100

    Scope search: update for capturing error

--- lib/libis/search/scope_search.rb
+++ lib/libis/search/scope_search.rb
@@ -9,6 +9,14 @@ class ScopeSearch < GenericSearch
 
   def query(term, index = nil, base = nil, options = {})
     OracleClient.scope_client.call('kul_packages.scope_xml_meta_file_ed', [term.upcase])
+    err_file = "/nas/vol03/oracle/scope01/#{term}_err.xml"
+    if File.exist? err_file
+      doc = XmlDocument.open(err_file)
+      msg = doc.xpath('/error/error_msg').first.content
+      msg_detail = doc.xpath('/error/error_').first.content
+      File.delete(err_file);
+      return nil
+    end
     doc = XmlDocument.open("/nas/vol03/oracle/scope01/#{term}_md.xml")
   end
 
